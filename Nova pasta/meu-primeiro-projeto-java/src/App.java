f9f6e79a72666e380206a75ae355a797a3d4f21f
Denzel Jhones <denzelxd09@icloud.com>
2024-05-01 17:57:00 -0300

    meu segundo projeto teste

--- Nova pasta/meu-primeiro-projeto-java/src/App.java
+++ Nova pasta/meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Boas vindas a Stormwind");
+        System.out.println("Boas vindas a Stormwin");
     }
 }
